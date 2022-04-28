1b9c9f49287a529afc967aed8f7635b310d6f322
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-04-28 11:51:33 -0500

    initial files created

---
+++ db/connection.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql2');
+
+
+//connect to mysql database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //your mysql username,
+        user: 'root',
+        //mysql password
+        password:'Gateway3!',
+        database: 'election'
+
+    },
+    console.log('Connected to the election database.')
+);
+
+module.exports = db;
