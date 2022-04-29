09ddde7d3a2df2c73d46a0d0ff1afac9ff9e575b
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-04-29 15:08:43 -0500

    added insert into and values into seeds sql

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO department (dept_id, dept_name)   
+VALUES                      
+(                                                    ),
+(                                                    );
+
+INSERT INTO roles ( roles_id, title, salary, dept_id)
+VALUES
+(                                                    ),
+(                                                    );
+INSERT INTO employee (emp-id, first_name, last_name, emp_role_id, manager_id)
+(                                                    ),
+(                                                    );
