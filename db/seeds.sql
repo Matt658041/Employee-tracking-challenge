82e15803496979ea0808b80aef9259749e04e9f2
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-04-29 15:13:45 -0500

    updated seeds sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,12 +1,21 @@
 INSERT INTO department (dept_id, dept_name)   
 VALUES                      
-(                                                    ),
-(                                                    );
+(1, "    "),
+(2, "    "),
+
 
 INSERT INTO roles ( roles_id, title, salary, dept_id)
 VALUES
-(                                                    ),
-(                                                    );
+(1, "    "),
+(2, "    "),
+(3, "    "),
+(4, "    "),
+
+
 INSERT INTO employee (emp-id, first_name, last_name, emp_role_id, manager_id)
-(                                                    ),
-(                                                    );
+(1, "    "),
+(2, "    "),
+(3, "    "),
+(4, "    "),
+(5, "    ");
+
